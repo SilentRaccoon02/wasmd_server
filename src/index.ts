071e8725b28c39adc27cc1e5d16382da4d7fc9df
SilentRaccoon02 <02smrnv@gmail.com>
2023-11-12 19:23:30 +0300

    Add test server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-// comment
-import add from './add.js';
-console.log(add(3, 4));
+import express, { type Application } from 'express';
+import path from 'path';
+
+const DIRNAME: string = path.resolve();
+const PORT: number = 2512;
+
+const app: Application = express();
+app.use(express.static(path.resolve(DIRNAME, '..', 'wasmd_web', 'build')));
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
