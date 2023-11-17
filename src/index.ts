308d00b213e3deb35868cf5295f17cd8a483363b
SilentRaccoon02 <02smrnv@gmail.com>
2023-11-17 10:11:27 +0300

    Add WebSocketServer

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import express, { type Application } from 'express';
+import express from 'express';
 import path from 'path';
+import type http from 'http';
+import type internal from 'stream';
+import { WebSocketServer, type WebSocket } from 'ws';
 
-const DIRNAME: string = path.resolve();
 const PORT: number = 2512;
+const BUILD_DIR: string = path.resolve();
+const STATIC_DIR: string = path.resolve(BUILD_DIR, '..', 'wasmd_web', 'build');
 
-const app: Application = express();
-app.use(express.static(path.resolve(DIRNAME, '..', 'wasmd_web', 'build')));
+const app: express.Application = express();
+const httpServer: http.Server = app.listen(PORT, () => { console.log(PORT); });
+const webSocketServer: WebSocketServer = new WebSocketServer({ noServer: true });
 
-app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+app.use(express.static(STATIC_DIR));
+
+httpServer.on('upgrade', (req: http.IncomingMessage, socket: internal.Duplex, head: Buffer) => {
+    webSocketServer.handleUpgrade(req, socket, head, (webSocket: WebSocket) => {
+        webSocketServer.emit('connection', webSocket, req);
+    });
 });
