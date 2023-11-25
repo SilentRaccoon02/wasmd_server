dfb1010e7679c9256e0bda427b2538781b50c535
SilentRaccoon02 <02smrnv@gmail.com>
2023-11-25 10:04:32 +0300

    Refactor project

--- src/Nodes.ts
+++ src/Nodes.ts
@@ -1,44 +1,45 @@
-import type DataObject from './DataObject.js';
-import { type RawData, type WebSocket } from 'ws';
-import { v4 as uuidv4 } from 'uuid';
+import { type WebSocket } from 'ws'
+import { v4 as uuidv4 } from 'uuid'
+
+interface DataObject {
+    type: string
+    from: string | undefined
+    to: string
+    data: any | undefined
+}
 
 export default class Nodes {
-    private readonly _nodes: Map<string, WebSocket>;
-
-    public constructor () {
-        this._nodes = new Map();
-    }
+    private readonly _nodes = new Map<string, WebSocket>()
 
     public readonly connection = (webSocket: WebSocket): void => {
-        const uuid: string = uuidv4();
-        const nodes: string[] = Array.from(this._nodes.keys());
+        const uuid = uuidv4()
+        const nodes = Array.from(this._nodes.keys())
 
-        webSocket.on('message', (message: RawData) => {
-            const jsonString: string = new TextDecoder().decode(message as ArrayBuffer);
-            const dataObject: DataObject = JSON.parse(jsonString);
-            this.send(dataObject);
-        });
+        webSocket.on('message', (message) => {
+            const jsonString = new TextDecoder().decode(message as ArrayBuffer)
+            const dataObject = JSON.parse(jsonString)
+            this.send(dataObject)
+        })
 
         webSocket.on('close', () => {
-            this._nodes.delete(uuid);
-
+            this._nodes.delete(uuid)
             for (const key of this._nodes.keys()) {
-                this.send({ type: 'close', from: undefined, to: key, data: uuid });
+                this.send({ type: 'close', from: undefined, to: key, data: uuid })
             }
-        });
+        })
 
-        webSocket.on('error', (error: Error) => { console.log(error); });
-        webSocket.binaryType = 'arraybuffer';
+        webSocket.on('error', (error) => { console.log(error) })
+        webSocket.binaryType = 'arraybuffer'
 
-        this._nodes.set(uuid, webSocket);
-        this.send({ type: 'uuid', from: undefined, to: uuid, data: undefined });
-        this.send({ type: 'nodes', from: undefined, to: uuid, data: nodes });
-    };
+        this._nodes.set(uuid, webSocket)
+        this.send({ type: 'uuid', from: undefined, to: uuid, data: undefined })
+        this.send({ type: 'nodes', from: undefined, to: uuid, data: nodes })
+    }
 
     private send (dataObject: DataObject): void {
-        console.log(`send: type: ${dataObject.type} from: ${dataObject.from} to: ${dataObject.to}`);
-        const jsonString: string = JSON.stringify(dataObject);
-        const bytes: Uint8Array = new TextEncoder().encode(jsonString);
-        this._nodes.get(dataObject.to)?.send(bytes);
+        console.log(`send: type: ${dataObject.type} from: ${dataObject.from} to: ${dataObject.to}`)
+        const jsonString = JSON.stringify(dataObject)
+        const bytes = new TextEncoder().encode(jsonString)
+        this._nodes.get(dataObject.to)?.send(bytes)
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import Nodes from './Nodes.js';
-import express from 'express';
-import path from 'path';
-import type http from 'http';
-import type internal from 'stream';
-import { WebSocketServer, type WebSocket } from 'ws';
+import Nodes from './Nodes.js'
+import express from 'express'
+import path from 'path'
+import { WebSocketServer } from 'ws'
 
-const PORT: number = 2512;
-const BUILD_DIR: string = path.resolve();
-const STATIC_DIR: string = path.resolve(BUILD_DIR, '..', 'wasmd_web', 'build');
+const PORT = 2512
+const BUILD_DIR = path.resolve()
+const STATIC_DIR = path.resolve(BUILD_DIR, '..', 'wasmd_web', 'build')
 
-const app: express.Application = express();
-const httpServer: http.Server = app.listen(PORT, () => { console.log(`running on port ${PORT}`); });
-const webSocketServer: WebSocketServer = new WebSocketServer({ noServer: true });
-const nodes: Nodes = new Nodes();
+const app = express()
+const httpServer = app.listen(PORT, () => { console.log(`running on port ${PORT}`) })
+const webSocketServer = new WebSocketServer({ noServer: true })
+const nodes = new Nodes()
 
-app.use(express.static(STATIC_DIR));
-webSocketServer.on('connection', nodes.connection);
+app.use(express.static(STATIC_DIR))
+webSocketServer.on('connection', nodes.connection)
 
-httpServer.on('upgrade', (req: http.IncomingMessage, socket: internal.Duplex, head: Buffer) => {
-    webSocketServer.handleUpgrade(req, socket, head, (webSocket: WebSocket) => {
-        webSocketServer.emit('connection', webSocket, req);
-    });
-});
+httpServer.on('upgrade', (req, socket, head) => {
+    webSocketServer.handleUpgrade(req, socket, head, (webSocket) => {
+        webSocketServer.emit('connection', webSocket, req)
+    })
+})
